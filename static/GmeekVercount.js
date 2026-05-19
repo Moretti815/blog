21ae199be6d0c3813c87e3ce46494aa0fdd87bf4
Mao Chenyu <chenyumao815@outlook.com>
2026-05-19 09:49:45 +0800

    1

--- static/GmeekVercount.js
+++ static/GmeekVercount.js
@@ -1,16 +1,26 @@
 function createVercount() {
-    var postBody = document.getElementById('postBody');
-    if (postBody){
-        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
-    }
-    var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>');
+  // 文章页面浏览量 - 插入到 post-meta-footer 中
+  var pageViewContainer = document.getElementById("busuanzi_container_page_pv");
+  if (pageViewContainer) {
+    pageViewContainer.style.display = "block";
+  }
+  // 网站总浏览量 - 插入到页脚
+  var runday = document.getElementById("runday");
+  if (runday) {
+    runday.insertAdjacentHTML(
+      "afterend",
+      '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>',
+    );
+  }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    createVercount();
-    var element = document.createElement('script');
-    element.src = 'https://vercount.one/js';
-    document.head.appendChild(element);
-    console.log("\n %c GmeekVercount Plugins https://github.com/Meekdai/Gmeek \n","padding:5px 0;background:#bc4c00;color:#fff");
+document.addEventListener("DOMContentLoaded", function () {
+  createVercount();
+  var element = document.createElement("script");
+  element.src = "https://vercount.one/js";
+  document.head.appendChild(element);
+  console.log(
+    "\n %c GmeekVercount Plugins https://github.com/Meekdai/Gmeek \n",
+    "padding:5px 0;background:#bc4c00;color:#fff",
+  );
 });
